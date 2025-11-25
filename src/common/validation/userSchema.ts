d03d8280b50fc3138985515f01fc6b641df0c017
Abshoor24 <muhammadnurulabshoor24@gmail.com>
2025-11-25 09:43:02 +0700

    update dikit

--- src/common/validation/userSchema.ts
+++ src/common/validation/userSchema.ts
@@ -12,7 +12,7 @@ export const createUserValidation = z.object({
 export const updateUserValidation = z.object({
   name: z.string().min(3, "*Required Name"),
   email: z.string().email("*Invalid email format"),
-  phone: z.string().min(10, "*Phone must be min 10 digits"),
-  address: z.string().min(1, "*Required Address"),
-    roleId: z.number().min(1, "Role is required"), 
+  phone: z.string().min(0),
+  address: z.string().min(0),
+  roleId: z.number().min(1, "Role is required"),
 });
